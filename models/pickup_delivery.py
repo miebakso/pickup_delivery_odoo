75ee5a7fa5896a413b50d19427abd49d53914a62
exzone97 <exzone97@yahoo.co.id>
2018-05-03 22:08:38 +0700

    Menambahkan Pickup delivery trip py & views

--- models/pickup_delivery.py
+++ models/pickup_delivery.py
@@ -53,3 +53,72 @@ class pickup_delivery_request_line(models.Model):
 	_sql_constraints = [
 		('qty','CHECK(qty > 0 )','Quantity must be greater than 0')
 	]
+
+# ==========================================================================================================================
+
+class pickup_delivery_trip(models.Model):
+    _name = "pickup.delivery.trip"
+    _description = "Pickup and delivery trip "
+
+    name = fields.Char('No. Trip', require=True)
+    courier_id = fields.Many2one('hr.employee', 'Courier', ondelete='restrict', require=True)
+    vehicle_id = fields.Many2one('fleet.vehicle', 'Vehicle', ondelete='restrict', require=True)
+    departure_date = fields.Datetime('Departure Date')
+    finished_date = fields.Datetime('Finished Date')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('ready', 'Ready'),
+        ('on_the_way', 'On The Way'),
+        ('finished', 'Finished'),
+        ('cancelled', 'Cancelled'),
+    ], 'State', required=True, default='draft')
+    trip_line_ids = fields.One2many('pickup.delivery.trip.line', 'trip_id', 'Trip Lines', required=True)
+
+    @api.one
+    def action_ready(self):
+        self.write({
+            'state': 'ready'
+        })
+
+    @api.one
+    def action_on_the_way(self):
+        self.write({
+            'state': 'on_the_way',
+            'departure_time': fields.Date.context_today(self),
+        })
+
+    @api.one
+    def action_finished(self):
+        self.write({
+            'state': 'finished',
+            'finished_time': fields.Date.context_today(self),
+        })
+
+    @api.one
+    def action_cancelled(self):
+        self.write({
+            'state': 'cancelled'
+        })
+
+# ==========================================================================================================================
+
+
+class pickup_delivery_trip_line(models.Model):
+    _name = "pickup.delivery.trip.line"
+    _description = "Pickup and Delivery Trip Line"
+
+    trip_id = fields.Many2one('pickup.delivery.trip', 'Trip', ondelete='cascade')
+    request_id = fields.Many2one('pickup.delivery.request', 'Request', ondelete='cascade', domain=[('state', '=', 'ready'), ('state', '=', 'delayed')])
+    request_desc = fields.Text('Description')
+    address = fields.Text('Address')
+    delivery_type = fields.Selection([
+        ('employee', 'Employee'),
+        ('outsource', 'Outsource'),
+    ], 'Delivery Type', required=True, default='employee')
+    # expedition_id =
+    notes = fields.Text('Notes')
+    execute_status = fields.Selection([
+        ('execute', 'Execute'),
+        ('not_execute', 'Not Execute'),
+    ], 'Execute Status')
+    partner_pic = fields.Char('PIC')
